f29e26c5082be0e986ebdc461dc0d7c57436eebe
delfick755 <delfick755@1b1aefb3-7854-0410-99b0-a9ef281f400a>
2009-04-16 14:49:37 +0000

    removing new tag

--- tags/new/filters/start.c
+++
@@ -1,9 +0,0 @@
-#include <windows.h>
-#include <shellapi.h>
-
-int WINAPI 
-WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow )
-{
-	ShellExecute( NULL, "open", lpCmdLine, NULL, NULL, SW_SHOWNORMAL );
-	return 0;
-}
